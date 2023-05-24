36f0aab26cc5ce679b60da9020498a87616d5398
shinoj-exe <76904393+shinoj-exe@users.noreply.github.com>
2023-05-24 17:01:52 +0530

    update

---
+++ blockchain/vending.js
@@ -0,0 +1,13 @@
+import Web3 from 'web3';
+
+const provider = new Web3.providers.HttpProvider(
+    "https://goerli.infura.io/v3/53b8cd509d7d431691e2747cb4c6d913"
+)
+
+const web3 = new Web3(provider)
+
+const abi = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"donutBalances","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getVendingMachineBalance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"amountOfDonuts","type":"uint256"}],"name":"purchase","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amountOfDonuts","type":"uint256"}],"name":"restock","outputs":[],"stateMutability":"nonpayable","type":"function"}]
+
+const vmContract = new web3.eth.Contract(abi,"Ox9ca89a0e3fba2abe5e075b130d30197690e75371")
+
+export default vmContract
